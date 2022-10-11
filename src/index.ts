4778ad88f034d04047d574ca8a53d01c549b500b
Guy Lawrance <guy.lawrance@manypets.com>
2022-10-11 13:50:40 +0100

    feat: update logging

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,12 @@ import { ManagementClient } from 'auth0'
 
 export async function run(): Promise<void> {
   try {
-    // TODO: change all console logs to info?
-    console.log('Starting action!')
     const auth0Domain = getInput('auth0-domain', { required: true })
     const auth0ClientId = getInput('auth0-client-id', { required: true })
     const auth0MgtClientId = getInput('auth0-management-client-id', { required: true })
     const auth0MgtClientSecret = getInput('auth0-management-client-secret', { required: true })
     const deployUrl = getInput('deploy-url', { required: true })
-    
 
-    console.log('Get Auth0 ManagementClient')
     const management = new ManagementClient({
       domain: auth0Domain,
       clientId: auth0MgtClientId,
@@ -27,37 +23,35 @@ export async function run(): Promise<void> {
       // scope: 'read:clients update:clients',
     })
       
-    console.log('Get Client')
     const client = await management.getClient({
       client_id: auth0ClientId,
     })
       
-    console.log('🗝 Retrieved Auth0 client:', client.name)
+    info('🗝 Retrieved Auth0 client: ' + client.name)
       
     const currentOrigins = client.web_origins || []
     const newOrigins = []
 
+    info('❓Checking for existence of URL: ' + deployUrl)
+
     if (currentOrigins.includes(deployUrl)) {
-      console.log(
-        '👍 Deploy Preview Prime URL has already been added to Auth0 client config'
+      info(
+        '👍 Deploy URL has already been added to Auth0 client config'
       )
-      } else {
-        console.log(
-        '👎 Deploy Preview Prime URL has not yet been added to Auth0 client config'
+    } else {
+      info(
+        '👎 Deploy URL has not yet been added to Auth0 client config'
       )
       newOrigins.push(deployUrl)
     }
 
     // If we don't have any URls to add, we can finish up early.
     if (!newOrigins.length) {
-      console.log('✅ Auth0 client config not in need of patch.')
+      info('✅ Auth0 client config not in need of patch.')
       return
     }
 
-    console.log(newOrigins)
-
   } catch (error) {
-    console.error(error)
     setFailed(JSON.stringify(error))
   }
 }
